4064647cb6f80be0d19dae9f214e4a681232a249
Kadeejath Salaha <codestudy475@gmail.com>
2024-11-01 15:54:17 +0530

    title update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   // Fun to add new task
   const addTask = () => {
     if (newTask.trim()) {
-      console.log("Adding task:", newTask);
+      console.log("Task Added:", newTask);
       setTasks([...tasks, { text: newTask, completed: false }]);
       setNewTask(""); // clear input field
     } else {
@@ -42,7 +42,7 @@ function App() {
   return (
     <div className="min-h-screen bg-blue-50 flex items-center justify-center p-4">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h1 className="text-2xl font-bold text-center mb-4">
+        <h1 className="text-2xl font-extrabold text-blue-950 text-center mb-4">
           Daily To-Do List
         </h1>
 
@@ -70,7 +70,7 @@ function App() {
             // Adding external css for checkbox
             <li
               key={index}
-              className="flex items-center gap-2 hover:bg-blue-100 transition duration-200 p-2 rounded" // Add hover and transition classes
+              className="flex items-center gap-2 p-2 rounded hover:text-blue-500 transition duration-200" // Add text hover and transition classes
             >
               <label className="custom-checkbox">
                 <input
@@ -90,7 +90,20 @@ function App() {
           ))}
         </ul>
 
+
         {tasks.length > 0 && (
+          <div className="flex justify-between items-center mt-4 text-gray-400 text-sm">
+            <p>{tasks.length} items</p>
+            <button
+              onClick={clearTasks}
+              className="hover:underline"
+            >
+              Clear All
+            </button>
+          </div>
+        )}
+        
+        {/* {tasks.length > 0 && (
           <button
             onClick={clearTasks}
             className="text-sm text-red-500 mt-4 hover:underline"
@@ -101,8 +114,8 @@ function App() {
 
         <div className="mt-4 text-center">
           <p>Total Tasks: {tasks.length}</p>{" "}
-          {/* Display total number of tasks */}
-        </div>
+          
+        </div> */}
       </div>
     </div>
   );
