8f510672c036469c0877c9db96e60de6a465a1b1
Kadeejath Salaha <codestudy475@gmail.com>
2024-11-01 11:49:00 +0530

    first div fine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 // import './index.css';
 
-
 function App() {
-  // const [tasks, setTasks]= useState([]);
-  // const [newTask, setNewTask]= useState("");
-
-
-  // const clearTasks= ()=>{
-  //   setTasks([]);
-  // }
+  
   return (
-    <div className='min-h-screen bg-blue-50 flex items-center justify-center p-4'>
-      <div className='bg-white p-6 rounded-lg shadow-lg max-w-md w-full'>
-        <h1 className='text-2x1 font-bold text-center mb-4'>Daily To-Do List</h1>
-        
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center p-4">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        <h1 className="text-2x1 font-bold text-center mb-4">
+          Daily To-Do List
+        </h1>
+
+        <div className="flex gap-2 mb-4">
+          <input
+            type="text"
+            placeholder="Add new list item"
+            className="border p-2 rounded w-full"
+          ></input>
+
+          <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">ADD</button>
+        </div>
+
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
