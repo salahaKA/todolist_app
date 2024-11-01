5237f054bf2b3fa94055cb135994373377026d32
Kadeejath Salaha <codestudy475@gmail.com>
2024-11-01 11:41:50 +0530

    sfgf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 // import './index.css';
 
+
 function App() {
+  // const [tasks, setTasks]= useState([]);
+  // const [newTask, setNewTask]= useState("");
+
+
+  // const clearTasks= ()=>{
+  //   setTasks([]);
+  // }
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-3xl">Hello, Tailwind!</h1>
+    <div className='min-h-screen bg-blue-50 flex items-center justify-center p-4'>
+      <div className='bg-white p-6 rounded-lg shadow-lg max-w-md w-full'>
+        <h1 className='text-2x1 font-bold text-center mb-4'>Daily To-Do List</h1>
+        
+      </div>
     </div>
   )
 }
